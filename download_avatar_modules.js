716751506d542fa414bc2bb942ecdd3a1034dee9
sam dunner <samdunner23@gmail.com>
2016-06-28 21:06:02 +0000

    versions with extensions in them

--- download_avatar_modules.js
+++ download_avatar_modules.js
@@ -1,9 +1,11 @@
 var request = require("request");
 require('dotenv').config();
 
-
 var username = process.env.DB_USER
 var password = process.env.DB_PASS
+  if(username === undefined || password === undefined) {
+  throw "Missing .env file, please fix naming or add link between files"
+  }
 
 function getRepoContributors(repoOwner, repoName, callback) {
   request({
@@ -14,8 +16,12 @@ function getRepoContributors(repoOwner, repoName, callback) {
     }
   },
   function(error, response, body) {
+    if (error)
+      throw "console.log error"
+    else if (response.statusCode !== 200)
+      throw "console.log response.statusCode"
     callback(error, JSON.parse(body));
-  });
+  })
 };
 
 module.exports = getRepoContributors;
--- download_avatars2.js
+++ download_avatars2.js
@@ -3,14 +3,22 @@ var fs = require("fs");
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
+if(process.argv.length !== 4) {
+  console.log("Incorrect number of arguments, use this format: node fileName.js repoOwner repoName");
+  return
+}
+
 var getRepoContributors = require("./download_avatar_modules.js");
 var downloadingavatars = require("./downloadingavatars.js");
 
 getRepoContributors(repoOwner, repoName, function(error, contributors) {
+
   if (error) { throw(error); }
 
+  fs.existsSync(repoName) || fs.mkdirSync(repoName);
+
   for (var contributor of contributors) {
-    fs.existsSync(repoName) || fs.mkdirSync(repoName);
+
     var filePath = `./${repoName}/` + contributor.login + ".png";
     downloadingavatars(contributor.avatar_url, filePath);
   }
